0ac31e222564d841daaffdbfbb0371d97e29a3ec
Ricardo Pardini <ricardo@pardini.net>
2024-04-26 22:20:08 +0200

    restore lost make-networkmanager-work code

--- fragments/ubuntu/networkmanager.sh
+++ fragments/ubuntu/networkmanager.sh
@@ -3,3 +3,11 @@
 function config_mkosi_pre::networkmanager() {
 	mkosi_config_add_rootfs_packages "network-manager" "modemmanager"
 }
+
+function mkosi_script_postinst_chroot::networkmanager() {
+	# let NetworkManager manage the network
+	touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+	ls -la /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+	sed "s/managed=\(.*\)/managed=true/g" -i /etc/NetworkManager/NetworkManager.conf
+	systemctl mask systemd-networkd.service
+}
