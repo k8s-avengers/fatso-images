a7d4e41f63cc8b66dd763c73794a8c00763decde
Ricardo Pardini <ricardo@pardini.net>
2024-08-28 12:37:02 +0200

    el/el_rocky_base: rocky-linux-europe-west4.production.gcp.mirrors.ctrliq.cloud is _only_ HTTPS capable

--- fragments/el/el_rocky_base.sh
+++ fragments/el/el_rocky_base.sh
@@ -2,6 +2,6 @@
 
 function config_mkosi_pre::rocky_mirror() {
 	mkosi_conf_begin_edit "mirror"
-	mkosi_conf_config_value "Distribution" "Mirror" "http://rocky-linux-europe-west4.production.gcp.mirrors.ctrliq.cloud/pub" # NO /rocky at the end
+	mkosi_conf_config_value "Distribution" "Mirror" "https://rocky-linux-europe-west4.production.gcp.mirrors.ctrliq.cloud/pub" # NO /rocky at the end
 	mkosi_conf_finish_edit "mirror"
 }
