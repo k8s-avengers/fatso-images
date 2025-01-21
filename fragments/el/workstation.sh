7215bdb8961c7d6dc4e478a98bc394391befa2dc
Ricardo Pardini <ricardo@pardini.net>
2025-01-21 19:45:55 +0100

    el: workstation: add pip/bat/pipx

--- fragments/el/workstation.sh
+++ fragments/el/workstation.sh
@@ -2,4 +2,6 @@
 
 function config_mkosi_pre::workstation_packages() {
 	mkosi_config_add_rootfs_packages "git"
+	mkosi_config_add_rootfs_packages "python3-devel" "python3-pip"
+	mkosi_config_add_rootfs_packages "bat" "pipx" # EPEL
 }
