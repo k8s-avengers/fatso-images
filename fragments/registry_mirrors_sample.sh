8092056ed235064b4afe4c8cc195f3545e448ce2
Ricardo Pardini <ricardo@pardini.net>
2025-01-22 16:13:53 +0100

    registry_mirrors_sample: fix wrong bash syntax for dict/associative array

--- fragments/registry_mirrors_sample.sh
+++ fragments/registry_mirrors_sample.sh
@@ -1,7 +1,7 @@
 function mkosi_script_postinst_chroot::100_setup_docker_registry_mirrors() {
 	log info "Setting up Docker registry mirrors with Harbor..."
-	docker_registry_prefixes+=(["quay.io"]="harbor.vmtest.pardini.net/quay.io")
-	docker_registry_prefixes+=(["docker.io"]="harbor.vmtest.pardini.net/docker.io")
+	docker_registry_prefixes["quay.io"]="harbor.vmtest.pardini.net/quay.io"
+	docker_registry_prefixes["docker.io"]="harbor.vmtest.pardini.net/docker.io"
 }
 
 function config_mkosi_init::predictable_image_names_sans_version() {
