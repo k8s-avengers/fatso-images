8e275c1a2d44b592fd14c22178de32465c2d984e
Ricardo Pardini <ricardo@pardini.net>
2025-01-22 13:54:23 +0100

    git-dev: new fragment for installing `git-credential-manager`

---
+++ fragments/git-dev.sh
@@ -0,0 +1,16 @@
+function mkosi_script_postinst_chroot::git_dev_git_credential_manager_install() {
+	# git-credential-manager
+	log info "Installing git-credential-manager version ${version}..."
+	declare version="2.6.1"
+	declare tarball_fn="gcm-linux_amd64.${version}.tar.gz"
+	declare down_url="https://github.com/git-ecosystem/git-credential-manager/releases/download/v${version}/${tarball_fn}"
+	wget --no-check-certificate -O "/tmp/${tarball_fn}" "${down_url}"
+	tar -xvf "/tmp/${tarball_fn}" -C /usr/local/bin
+	rm -f "/tmp/${tarball_fn}"
+
+}
+
+function mkosi_script_finalize_chroot::git_dev_git_credential_manager_check() {
+	log info "Check git-credential-manager version..."
+	/usr/local/bin/git-credential-manager --version
+}
