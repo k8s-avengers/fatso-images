946e03cb21cd3aabfa8d4c22849b386e400d2fd9
Ricardo Pardini <ricardo@pardini.net>
2025-01-22 12:44:27 +0100

    el/el9_base: add `btrfs-progs` and `zstd` to all images

--- fragments/el/el9_base.sh
+++ fragments/el/el9_base.sh
@@ -10,6 +10,7 @@ function config_mkosi_pre::el_9_base_pkgs() {
 		bash
 		bash-completion
 		xfsprogs
+		btrfs-progs # EPEL
 		sysstat
 		sudo
 		bc
@@ -37,6 +38,7 @@ function config_mkosi_pre::el_9_base_pkgs() {
 		openssh-clients
 		openssh-server
 		hostname
+		zstd
 	)
 	mkosi_config_add_rootfs_packages "${pkgs[@]}"
 }
