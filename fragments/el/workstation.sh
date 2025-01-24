507756278fae8cfd6530cabd63784ddac024a1b1
Ricardo Pardini <ricardo@pardini.net>
2025-01-24 13:26:59 +0100

    el/workstation: add pkgs vim-enhanced, vim-filesystem and crudini

--- fragments/el/workstation.sh
+++ fragments/el/workstation.sh
@@ -2,6 +2,8 @@
 
 function config_mkosi_pre::workstation_packages() {
 	mkosi_config_add_rootfs_packages "git"
+	mkosi_config_add_rootfs_packages "vim-enhanced" "vim-filesystem"
+	mkosi_config_add_rootfs_packages "crudini" # EPEL
 	mkosi_config_add_rootfs_packages "python3-devel" "python3-pip"
 	mkosi_config_add_rootfs_packages "bat" "pipx" # EPEL
 }
