8e35f00adeb97f6333bec6b942a916d68d431881
Ricardo Pardini <ricardo@pardini.net>
2025-07-20 12:28:52 +0200

    inventory_vendor_ka: drop fragments, replace with inline hooks in ka inventory

--- flavors/inventory_vendor_ka.sh
+++ flavors/inventory_vendor_ka.sh
@@ -5,14 +5,27 @@ function flavor_vendor_standard() {
 	:
 }
 
+# k8s-avengers vendor flavor. do NOT use these in production!
 function flavor_vendor_ka() {
-	FLAVOR_FRAGMENTS+=(
-		"serial_console"
-		"registry_mirrors_sample"
-		"add_ansible_user"
-	)
+
+	function config_mkosi_init::predictable_image_names_sans_version() {
+		log warn "Using predictable dist image name: '${FLAVOR}_${OS_ARCH}'"
+		MKOSI_CONTENT_ENVIRONMENT["IMAGE_FLAVOR_VERSION_ID"]="${FLAVOR}_${OS_ARCH}"
+	}
+
+	FLAVOR_FRAGMENTS+=("serial_console") # Enable ttyS0 serial console
 
 	case "${FLAVOR_DISTRO_TYPE}" in
-		"apt") FLAVOR_FRAGMENTS+=("apt/ssh-import-id") ;; # Only deb-based distros have this
+		"apt") # Only deb-based distros have this
+			function config_mkosi_pre::ssh_import_id() {
+				log warn "Enabling ssh-import-id and a hardcoded SSH public key - DO NOT use in production"
+				mkosi_config_add_rootfs_packages "ssh-import-id" #"python3-launchpadlib"
+			}
+
+			function mkosi_script_postinst_chroot::ssh_keys() {
+				log warn "Enabling ssh-import-id and a hardcoded SSH public key - DO NOT use in production"
+				ssh-import-id gh:rpardini
+			}
+			;;
 	esac
 }
