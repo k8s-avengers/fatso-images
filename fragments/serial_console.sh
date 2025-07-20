b97eafbbd5dedb1bb301d45aaf5040ca6aa748de
Ricardo Pardini <ricardo@pardini.net>
2025-07-20 12:28:09 +0200

    serial_console: handle ttyS0/ttyAMA0

--- fragments/serial_console.sh
+++ fragments/serial_console.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 function config_mkosi_pre::serial_console() {
-	log warn "Enabling serial console on ttyS0"
-	KERNEL_CMDLINE_FRAGMENTS+=("console=ttyS0")
+	log warn "Enabling serial console..."
+	case "${OS_ARCH}" in
+		"amd64")
+			log info "Enabling serial console on ttyS0 for x86_64"
+			KERNEL_CMDLINE_FRAGMENTS+=("console=ttyS0")
+			;;
+		"arm64")
+			log info "Enabling serial console on ttyAMA0 for aarch64"
+			KERNEL_CMDLINE_FRAGMENTS+=("console=ttyAMA0")
+			;;
+		*)
+			log error "Unsupported architecture '${OS_ARCH}' for serial console configuration."
+			exit 1
+			;;
+	esac
 }
