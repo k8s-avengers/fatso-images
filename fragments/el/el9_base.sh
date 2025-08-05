d08c377d4d1b9176e9fa63949ff4a7c679d0b557
Ricardo Pardini <ricardo@pardini.net>
2025-08-05 10:12:02 +0200

    el/el9_base: add EL_RELEASE to the mkosi environment (so it can be used in postinst, etc)

--- fragments/el/el9_base.sh
+++ fragments/el/el9_base.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 function config_mkosi_pre::el_9_base_pkgs() {
+	# Pass down EL_RELEASE to the mkosi environment (postinst, etc)
+	MKOSI_CONTENT_ENVIRONMENT["EL_RELEASE"]="${EL_RELEASE}"
+
 	declare -a pkgs=(
 		vim-minimal
 		nano
